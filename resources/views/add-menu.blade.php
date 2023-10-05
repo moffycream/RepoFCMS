2a7b101c68f3a658f4186d6815f0acdfc769b38a
AustinCKR <austinckr1229@gmail.com>
2023-10-05 21:27:40 +0800

    new file:   resources/views/add-menu.blade.php

---
+++ resources/views/add-menu.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+@include ('include/header')
+
+<body>
+<article>
+    <h1>Add Menu</h1>
+</article>
+</body>
+
+@include ('include/footer')
+</html>
