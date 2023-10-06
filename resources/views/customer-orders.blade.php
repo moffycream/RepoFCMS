431f31105605b9a7f888aca937cad08ea9eaacff
Kai0220 <wenkai0638@gmail.com>
2023-10-06 19:11:47 +0800

    modified:   app/Http/Controllers/Controller.php 	new file:   resources/views/customer-orders.blade.php modified:   resources/views/include/header.blade.php 	modified:   resources/views/profile.blade.php

---
+++ resources/views/customer-orders.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include ('include/head')
+
+@include ('include/header')
+
+<article>
+    <h1 class="profile-h1">Customer Order Listing</h1>
+    <div class="profile-container">       
+            </div>
+        </div>
+    </article>
+@include ('include/footer')
+</html>
+
+<!-- Credit https://www.nicepng.com/downpng/u2y3a9e6t4o0a9w7_profile-picture-default-png/*/  -->
