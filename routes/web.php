ba79df411437c59cc3f25b016eb8f3ea80d8dc68
moffycream <106477441+moffycream@users.noreply.github.com>
2023-10-07 14:41:21 +0800

    new file:   database/migrations/2023_10_07_063826_create_order_menu_table.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ use App\Http\Controllers\ProfileController;
 
 // operation routes
 Route::get('/op-orders', [OrderController::class, 'index']);
+Route::post('/op-orders/{orderID}', [OrderController::class, 'viewOrder'])->name('viewOrder');
+
 Route::get('/profile',[ProfileController::class, 'retrieveInfo']);
 Route::get('/add-food', [FoodController::class, 'index']);
 Route::post('/add-food', [FoodController::class, 'registerNewFood'])->name('food.register');
