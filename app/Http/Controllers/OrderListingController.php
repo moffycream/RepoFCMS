b3aad410ae4aadd3561fb6239e0c45a79f8605ce
Kai0220 <wenkai0638@gmail.com>
2023-10-09 17:34:41 +0800

    modified:   app/Http/Controllers/OrderListingController.php 	modified:   public/css/jesse.css 	modified:   public/js/app.js 	modified:   resources/views/customer-orders-listings.blade.php 	modified:   resources/views/customer-orders.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/OrderListingController.php
+++ app/Http/Controllers/OrderListingController.php
@@ -14,13 +14,18 @@ class OrderListingController extends Controller
     }
 
     public function viewOrderDetails($orderID)
-{
-    $selectedOrder = Order::find($orderID);
-
-    return view('customer-orders-listings', ['selectedOrder' => $selectedOrder]);
-}
-
+    {
+        $selectedOrder = Order::find($orderID);
     
+        return view('customer-orders-listings', ['selectedOrder' => $selectedOrder]);
+    }
 
-
+    public function cancelOrder($orderID)
+    {
+        // Retrieve the order by ID
+        $order = Order::find($orderID);
+        $order->delete();
+        $order_list = Order::all();
+        return view('customer-orders', ['orders' => $order_list]);
+    }
 }
