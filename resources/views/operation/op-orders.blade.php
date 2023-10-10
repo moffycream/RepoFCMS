85e529389e6fe4fde61d1d198942277737d64023
moffycream <106477441+moffycream@users.noreply.github.com>
2023-10-10 19:17:09 +0800

    modified:   app/Http/Controllers/LoginController.php 	modified:   public/css/gavin.css 	modified:   public/css/kelvin.css 	modified:   resources/views/operation/op-orders.blade.php

--- resources/views/operation/op-orders.blade.php
+++ resources/views/operation/op-orders.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Orders')
 @section('content')
-
-<p class="dummy-op-orders"></p>
 <div class="container-op-orders">
     <div class="panel">
         <div class="row-op-orders">
@@ -38,48 +36,4 @@
         </table>
     </div>
 </div>
-<p class="dummy-op-orders"></p>
 @endsection
-
-{{-- <div class="container-op-orders">
-    <div class="container">
-        <h1>Order List</h1>
-        @foreach($orders as $order)
-        <div class="row-order-item">
-            <form method="post" action="{{route('op.order-view', $order->orderID)}}">
-{{ csrf_field() }}
-<button type="submit">
-    <div class="row">
-        <p>Order #{{$order->orderID}}</p>
-        <p>{{$order->getformattedDateTime()}}</p>
-    </div>
-    <p class="col">RM{{$order->getTotalPrice()}}</p>
-</button>
-</form>
-</div>
-@endforeach
-</div>
-<div class="container">
-    @if(isset($selectedOrder))
-    <h2>Order Details</h2>
-    <div class="row-details">
-        <p>Name: {{$selectedOrder->name}}</p>
-        <p>Contact: {{$selectedOrder->contact}}</p>
-        <p>Address: {{$selectedOrder->address}}</p>
-        <p>Status: {{$selectedOrder->status}}</p>
-    </div>
-    
-    <div class="row-addon">
-        <h3>Add-Ons</h3>
-        <p>{{$selectedOrder->order_notes}}</p>
-    </div>
-    <div class="row-price">
-        <p>Total Price: RM{{$selectedOrder->getTotalPrice()}}</p>
-    </div>
-    <div class="row-action">
-        <a href="{{route('op.order-cancel', $selectedOrder->orderID)}}">Cancel Order</a>
-        <a>Accept Order</a>
-    </div>
-    @endif
-</div>
-</div> --}}
