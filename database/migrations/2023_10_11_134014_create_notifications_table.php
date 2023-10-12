4bc70c82f035c51bff2e06f2e9a286b296abae55
moffycream <106477441+moffycream@users.noreply.github.com>
2023-10-12 13:55:03 +0800

    modified:   database/migrations/2023_10_11_134014_create_notifications_table.php 	modified:   public/css/style.css 	modified:   resources/views/include/header.blade.php

--- database/migrations/2023_10_11_134014_create_notifications_table.php
+++ database/migrations/2023_10_11_134014_create_notifications_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id('notificationID');
             $table->char('content');
+            $table->boolean('isRead')->default(false);
             $table->timestamps();
         });
     }
