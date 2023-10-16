787db3000b100e91948f69929e7826b97fea0b37
moffycream <106477441+moffycream@users.noreply.github.com>
2023-10-16 13:05:18 +0800

    modified:   public/css/gavin.css 	modified:   resources/views/admin/admin-register.blade.php

--- resources/views/admin/admin-register.blade.php
+++ resources/views/admin/admin-register.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'Dashboard')
 @section('content')
 <div class="container-admin-register-page">
-    <img class="admin-register-page-logo" src="{{ asset('images/logo.png') }}" alt="logo">
     <form method="post" action="{{route('admin.register')}}">
         @csrf
         @if(isset($errorMsg) && !empty($errorMsg))
@@ -20,33 +19,36 @@
                 <div class="admin-register-left-column">
                     <h2>Sign up</h2>
                     <p>Start an account, to start your journey</p>
-                    <input type="text" id="username" name="username" placeholder="Username" value="{{isset($_POST['username']) ? $_POST['username'] : ''}}" required><br><br>
-                    <input type="password" id="password" name="password" placeholder="Password" required><br><br>
-                    <input type="password" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password" required><br><br>
-                    <input type="text" id="email" name="email" placeholder="Email" value="{{isset($_POST['email']) ? $_POST['email'] : ''}}" required><br><br>
+                    <div>
+                        <input type="text" id="username" name="username" placeholder="Username" value="{{isset($_POST['username']) ? $_POST['username'] : ''}}" required>
+                        <input type="password" id="password" name="password" placeholder="Password" required>
+                        <input type="password" id="confirmpassword" name="confirmpassword" placeholder="Confirm Password" required>
+                        <input type="text" id="email" name="email" placeholder="Email" value="{{isset($_POST['email']) ? $_POST['email'] : ''}}" required>
+                    </div>
                 </div>
 
                 <div class="admin-register-right-column">
                     <h2>Let us know more!</h2>
                     <p>Ensure a smooth food journey</p>
-                    <input type="text" id="firstName" name="firstName" placeholder="First Name" value="{{isset($_POST['firstName']) ? $_POST['firstName'] : ''}}" required><br><br>
-                    <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="{{isset($_POST['lastName']) ? $_POST['lastName'] : ''}}" required><br><br>
-                    <input type="text" id="phone" name="phone" placeholder="Phone number" value="{{isset($_POST['phone']) ? $_POST['phone'] : ''}}" required><br><br>
-                    <textarea id="streetAddress" name="streetAddress" placeholder="Street Address" value="{{isset($_POST['streetAddress']) ? $_POST['streetAddress'] : ''}}" required></textarea><br><br>
-                    <input type="text" id="city" name="city" placeholder="City" value="{{isset($_POST['city']) ? $_POST['city'] : ''}}" required><br><br>
-                    <input type="text" id="postcode" name="postcode" placeholder="Postcode" value="{{isset($_POST['postcode']) ? $_POST['postcode'] : ''}}" required><br><br>
+                    <div>
+                        <input type="text" id="firstName" name="firstName" placeholder="First Name" value="{{isset($_POST['firstName']) ? $_POST['firstName'] : ''}}" required>
+                        <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="{{isset($_POST['lastName']) ? $_POST['lastName'] : ''}}" required>
+                        <input type="text" id="phone" name="phone" placeholder="Phone number" value="{{isset($_POST['phone']) ? $_POST['phone'] : ''}}" required>
+                        <textarea id="streetAddress" name="streetAddress" placeholder="Street Address" value="{{isset($_POST['streetAddress']) ? $_POST['streetAddress'] : ''}}" required></textarea>
+                        <input type="text" id="city" name="city" placeholder="City" value="{{isset($_POST['city']) ? $_POST['city'] : ''}}" required>
+                        <input type="text" id="postcode" name="postcode" placeholder="Postcode" value="{{isset($_POST['postcode']) ? $_POST['postcode'] : ''}}" required>
+                    </div>
                 </div>
             </div>
 
             <div class="admin-register-centre-column">
-                <select name="accountType" id="accountType">
+                <select name="accountType" id="accountType" title="select account type">
                     <optgroup label="Choose account type">
                         <option value="Customer">Customer</option>
                         <option value="OperationTeam">Operation Team</option>
                         <option value="Admin">Admin</option>
                     </optgroup>
                 </select>
-                <br><br>
                 <button class="admin-register-submit-button" type="submit">Create account</button>
             </div>
         </div>
