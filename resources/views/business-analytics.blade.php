a55ce98022140c091958d256b99cb55479b20a50
WongUng01 <wongzingung2242@gmail.com>
2023-10-19 23:34:34 +0800

    modified:   app/Http/Controllers/AnalyticsController.php 	new file:   public/menu-images/1697694442Picture1.png 	new file:   public/menu-images/1697694455Picture1.png 	modified:   resources/views/business-analytics.blade.php

--- resources/views/business-analytics.blade.php
+++ resources/views/business-analytics.blade.php
@@ -10,25 +10,13 @@
 <script>
     var ctx = document.getElementById('revenueChart').getContext('2d');
 
-    // Function to generate random data
-    function generateRandomData(count) 
-    {
-        var data = [];
-        for (var i = 0; i < count; i++) 
-        {
-            data.push(Math.floor(Math.random() * 1000)); // Change the range and logic as needed
-        }
-        return data;
-    }
-
-    var chartData = 
-    {
+    // Your chart data from the server
+    var chartData = {
         labels: <?php echo json_encode($chartData['labels']); ?>,
-        datasets: 
-        [
+        datasets: [
             {
                 label: 'Revenue',
-                data: generateRandomData(<?php echo count($chartData['labels']); ?>), // Generate random data
+                data: <?php echo json_encode($chartData['data']); ?>, // Use the actual data from your Laravel controller
                 backgroundColor: 'rgba(75, 192, 192, 0.2)', // Customize the chart colors
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
@@ -36,16 +24,14 @@
         ]
     };
 
-    var chart = new Chart(ctx, 
-    {
+    console.log(chartData);
+
+    var chart = new Chart(ctx, {
         type: 'bar', // Change the chart type as needed
         data: chartData,
-        options: 
-        {
-            scales: 
-            {
-                y: 
-                {
+        options: {
+            scales: {
+                y: {
                     beginAtZero: true
                 }
             }
