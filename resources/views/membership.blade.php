9bcea8e9d79ca81dbac9db40f9562fe9d0c2a4d6
SanaPotter03 <145409481+SanaPotter03@users.noreply.github.com>
2023-11-13 19:46:37 +0800

    modified:   public/css/guilbert.css 	modified:   resources/views/membership.blade.php

--- resources/views/membership.blade.php
+++ resources/views/membership.blade.php
@@ -3,15 +3,29 @@
 @section('content')
 
 <div class="membership-container">
+
     <h1>Membership</h1>
 
     <div class="tier-container">
+        
+        <section id="tier0">
+            <h2>Tier 0 Membership</h2>
+            <p>Tier 0 Benefits: </p>
+            <ul class="benefit-list">
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Default Membership</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> No Discount</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> No Spending Minimum Required</li>
+
+            </ul>        
+        </section>
+
         <section id="tier1">
             <h2>Tier 1 Membership</h2>
             <p>Tier 1 Benefits: </p>
             <ul class="benefit-list">
-                <li class="benefit-item">&#10004; Discount: RM 25</li>
-                <li class="benefit-item">&#10004; Minimum Total Spent: RM 500</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Discount: RM 25</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Capped to 5 Discount Every Month</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Minimum Total Spent: RM 300</li>
             </ul>        
         </section>
         
@@ -19,8 +33,9 @@
             <h2>Tier 2 Membership</h2>
             <p>Tier 2 Benefits: </p>
             <ul class="benefit-list">
-                <li class="benefit-item">&#10004; Discount: RM 30</li>
-                <li class="benefit-item">&#10004; Minimum Total Spent: RM 1000</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Discount: RM 30</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Capped to 5 Discount Every Month</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Minimum Total Spent: RM 600</li>
             </ul>  
         </section>
 
@@ -28,22 +43,22 @@
             <h2>Tier 3 Membership</h2>
             <p>Tier 3 Benefits: </p>
             <ul class="benefit-list">
-                <li class="benefit-item">&#10004; Discount: RM 40</li>
-                <li class="benefit-item">&#10004; Minimum Total Spent: RM 2000</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Discount: RM 40</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Capped to 5 Discount Every Month</li>
+                <li class="benefit-item"><span style="color: #90EE90;">&#10004</span> Minimum Total Spent: RM 1000</li>
             </ul>  
         </section>
     </div>
-    
-    <aside>
 
+    <aside id='membership-aside'>
         {{--Display Total Spending--}}
         @if(isset($totalAmountPaid))
-            <h2>Your Total Spent: RM{{ $totalAmountPaid }}</h2>
-            <h2>Current Tier Level: {{ $tier }}</h2>
-            <p>Discount Amount: {{ $discountAmount }}</p>
+            <p><strong>Your Total Spent:</strong> RM{{ $totalAmountPaid }}</p>
+            <p><strong>Current Tier Level:</strong> {{ $tier }}</p>
+            <p><strong>Discount Amount:</strong> {{ $discountAmount }}</p>
         @endif
-
     </aside>
+
 </div>
 
 @endsection
